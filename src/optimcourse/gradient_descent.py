debug restarted gradient and correct np.array into np.ndarray when talking about a type

src/optimcourse/gradient_descent.py
@@ -11,8 +11,8 @@ from optimcourse.optim_utilities import record_any
 
 #%% finite difference function
 def gradient_finite_diff(
-    func: object, x: np.array, f_x: float, epsilon: float = 1e-7
-) -> np.array:
+    func: object, x: np.ndarray, f_x: float, epsilon: float = 1e-7
+) -> np.ndarray:
     """
     Gradient estimation by forward finite difference
 
@@ -22,7 +22,7 @@ def gradient_finite_diff(
     ----------
     func : object
         function.
-    x : np.array
+    x : np.ndarray
         point where the gradient is approximated.
     f_x : float
             function value at x.
@@ -31,7 +31,7 @@ def gradient_finite_diff(
 
     Returns
     -------
-    gradient : 1D np.array
+    gradient : 1D np.ndarray
         estimate of the 1D gradient of the partial derivatives at x.
 
     """
@@ -48,10 +48,10 @@ def gradient_finite_diff(
 
 #%% line search
 def linesearch(
-    x: np.array,
+    x: np.ndarray,
     f_x: float,
-    gradf: np.array,
-    direction: np.array,
+    gradf: np.ndarray,
+    direction: np.ndarray,
     func: object,
     LB: List,
     UB: List,
@@ -75,13 +75,13 @@ def linesearch(
 
     Parameters
     ----------
-    x : np.array
+    x : np.ndarray
         current point.
     f_x : float
         objective function at x.
-    gradf : np.array
+    gradf : np.ndarray
         gradient of f at x.
-    direction : np.array
+    direction : np.ndarray
         proposed search direction. Not necessarily a descent direction
         (hence works with momentum and NAG).
     func : object
@@ -109,7 +109,7 @@ def linesearch(
 
     Returns
     -------
-    next_x : np.array
+    next_x : np.ndarray
         next point found.
     n_loop-1
         cost of line search.
@@ -184,9 +184,9 @@ def linesearch(
 #%% gradient-based descent algos, work in progress. TODO : NAG, better documentation
 def gradient_descent(
     func: object,
-    start_x: np.array,
-    LB: np.array,
-    UB: np.array,
+    start_x: np.ndarray,
+    LB: np.ndarray,
+    UB: np.ndarray,
     budget: int = 1e3,
     step_factor: float = 1e-1,
     direction_type: str = "momentum",

src/optimcourse/optim_utilities.py
@@ -16,7 +16,7 @@ from typing import Callable, List
 
 
 def record_hist(
-    rec: dict, f: float, x: np.array, time: int, key_f: str, key_x: str, key_time: str
+    rec: dict, f: float, x: np.ndarray, time: int, key_f: str, key_x: str, key_time: str
 ) -> dict:
     """generic function to update history records"""
     if key_f not in rec:
@@ -36,7 +36,7 @@ def record_hist(
 
 
 def record_best(
-    rec: dict, fbest: float, xbest: np.array, time: int, printlevel: int
+    rec: dict, fbest: float, xbest: np.ndarray, time: int, printlevel: int
 ) -> dict:
     """create and update records of best so far"""
     rec["x_best"] = xbest
@@ -55,7 +55,7 @@ def record_best(
     return rec
 
 
-def record_any(rec: dict, f: float, x: np.array, time: int, printlevel: int) -> dict:
+def record_any(rec: dict, f: float, x: np.ndarray, time: int, printlevel: int) -> dict:
     """create and update record of any point during the search"""
     rec["time_used"] = time
     if printlevel > 1:

src/optimcourse/restarted_gradient_descent.py
@@ -6,9 +6,9 @@ from optimcourse.gradient_descent import gradient_descent
 
 def restarted_gradient_descent(
     func: object,
-    start_x: np.array = None,
-    LB: np.array = None,
-    UB: np.array = None,
+    start_x: np.ndarray = np.empty(0,),
+    LB: np.ndarray = None,
+    UB: np.ndarray = None,
     budget: int = 1e3,
     nb_restarts: int = 4,
     step_factor: float = 1e-1,
@@ -35,7 +35,7 @@ def restarted_gradient_descent(
     cum_time=0
     for iter in range(1,(nb_restarts+1)):
         # get the starting point
-        if iter==1 and start_x.any()!=False:
+        if iter == 1 and not(start_x.shape==(0,)):
             one_start_x = start_x
         else:
             one_start_x = np.random.uniform(low=LB, high=UB)